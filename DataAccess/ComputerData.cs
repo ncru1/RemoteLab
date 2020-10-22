d2b2214e9ba2c5fd3109366cef66d1e36532912f
Nick C <ncrutch214@gmail.com>
2020-10-22 14:36:09 -0400

    links

--- DataAccess/ComputerData.cs
+++ DataAccess/ComputerData.cs
@@ -17,21 +17,21 @@ namespace DataAccess
 
         public Task<List<ComputerModel>> GetComputer()
         {
-            string sql = "SELECT * from dbo.test";
+            string sql = "SELECT * from dbo.RemoteLab";
 
             return _db.LoadData<ComputerModel, dynamic>(sql, new { });
         }
 
         public Task InsertComputer(ComputerModel computer)
         {
-            string sql = @"INSERT INTO dbo.test (FQDN, RDPStatus) VALUES (@FQDN, @RDPStatus);";
+            string sql = @"INSERT INTO dbo.RemoteLab (FQDN, RDPStatus) VALUES (@FQDN, @RDPStatus);";
 
             return _db.SaveData(sql, computer);
         }
 
         public Task UpdateComputer(ComputerModel computer)
         {
-            string sql = @"UPDATE dbo.test SET RDPStatus = @RDPStatus WHERE FQDN = @FQDN;";
+            string sql = @"UPDATE dbo.RemoteLab SET RDPStatus = @RDPStatus WHERE FQDN = @FQDN;";
 
             return _db.SaveData(sql, computer);
         }
